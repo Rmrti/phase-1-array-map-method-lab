47fb0e535229e3898366f834ab2e3313ca63034d
Rmrti <mutuma34@gmail.com>
2024-12-06 17:51:26 +0300

    Add function to capitalize the first letter of each word in an array of tutorial titles

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((line) =>
+    line
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(" ")
+  );
+};
+
